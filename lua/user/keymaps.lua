8600100ed8c9a0b8e48b132d15ddb3278e5ac6ff
Gary Zhang <garyzhang2002@hotmail.com>
2022-07-08 21:01:21 +0800

    set <Space> as the leader key; add a key binding to toggle nvim-tree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,12 +1,16 @@
 local keymap = vim.api.nvim_set_keymap
 local no_re_opts = {noremap = true, silent = true}
 
+vim.g.mapleader = " "
+
 -- move line(s) up or down
 keymap("n", "<A-j>", ":silent! move .+1<CR>", no_re_opts) -- use silent! to avoid error messages when moving over boundaries
 keymap("n", "<A-k>", ":silent! move .-2<CR>", no_re_opts)
-keymap("v", "<A-j>", "<ESC>:silent! '<,'>move '>+1<CR>gv", no_re_opts)
-keymap("v", "<A-k>", "<ESC>:silent! '<,'>move '<-2<CR>gv", no_re_opts)
+keymap("v", "<A-j>", "<Esc>:silent! '<,'>move '>+1<CR>gv", no_re_opts)
+keymap("v", "<A-k>", "<Esc>:silent! '<,'>move '<-2<CR>gv", no_re_opts)
 
 -- stay in visual mode after indenting so that consecutive indenting is allowed
 keymap("v", ">", ">gv", no_re_opts)
 keymap("v", "<", "<gv", no_re_opts)
+
+keymap("n", "<C-b>", ":NvimTreeToggle<CR>", no_re_opts)
--- lua/user/plugins/lsp/nvim-lspconfig.lua
+++ lua/user/plugins/lsp/nvim-lspconfig.lua
@@ -2,10 +2,10 @@ local keymap = vim.keymap.set
 
 -- see `:help vim.diagnostic.*` for documentation on any of the below functions
 local no_re_opts = {noremap = true, silent = true}
-keymap("n", "<space>e", vim.diagnostic.open_float, no_re_opts)
+keymap("n", "<Leader>e", vim.diagnostic.open_float, no_re_opts)
 keymap("n", "[d", vim.diagnostic.goto_prev, no_re_opts)
 keymap("n", "]d", vim.diagnostic.goto_next, no_re_opts)
-keymap("n", "<space>q", vim.diagnostic.setloclist, no_re_opts)
+keymap("n", "<Leader>q", vim.diagnostic.setloclist, no_re_opts)
 
 -- use an on_attach function to only map the following keys after the language server attaches to the current buffer
 local on_attach = function(client, buffer_number)
@@ -16,14 +16,14 @@ local on_attach = function(client, buffer_number)
     keymap("n", "K", vim.lsp.buf.hover, no_re_buf_opts)
     keymap("n", "gi", vim.lsp.buf.implementation, no_re_buf_opts)
     keymap("n", "<C-k>", vim.lsp.buf.signature_help, no_re_buf_opts)
-    keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, no_re_buf_opts)
-    keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, no_re_buf_opts)
-    keymap("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, no_re_buf_opts)
-    keymap("n", "<space>D", vim.lsp.buf.type_definition, no_re_buf_opts)
-    keymap("n", "<space>rn", vim.lsp.buf.rename, no_re_buf_opts)
-    keymap("n", "<space>ca", vim.lsp.buf.code_action, no_re_buf_opts)
+    keymap("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, no_re_buf_opts)
+    keymap("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, no_re_buf_opts)
+    keymap("n", "<Leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, no_re_buf_opts)
+    keymap("n", "<Leader>D", vim.lsp.buf.type_definition, no_re_buf_opts)
+    keymap("n", "<Leader>rn", vim.lsp.buf.rename, no_re_buf_opts)
+    keymap("n", "<Leader>ca", vim.lsp.buf.code_action, no_re_buf_opts)
     keymap("n", "gr", vim.lsp.buf.references, no_re_buf_opts)
-    keymap("n", "<space>f", vim.lsp.buf.formatting, no_re_buf_opts)
+    keymap("n", "<Leader>f", vim.lsp.buf.formatting, no_re_buf_opts)
 end
 
 local lsp_flags = {
