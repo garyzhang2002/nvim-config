5139e61c6cfd304094184a4b1e9f9921dcf12520
Gary Zhang <garyzhang2002@hotmail.com>
2022-07-17 22:39:33 +0800

    modify the running files feature

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 require("toggleterm").setup {
     open_mapping = "<C-\\>",
     direction = "float",
-    close_on_exit = false,
+    close_on_exit = true,
     float_opts = {
 		border = "curved",
 		winblend = 10,
@@ -10,25 +10,33 @@ require("toggleterm").setup {
 
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
+
 local keymap = vim.api.nvim_set_keymap
 local no_re_opts = {noremap = true, silent = true}
 
-local cmd_prefix = ":w | ToggleTerm dir=%:p:h<CR>"
-local cmd_suffix = " ; exit<CR>"
 augroup("run_current_file", {clear = true})
 autocmd("BufEnter", {
     group = "run_current_file",
-    pattern = "*",
+    pattern = "*.*", -- exclude buffers for stuff like diagnostics
     callback = function ()
-        local file = vim.fn.expand("%:p")
         local ext = vim.fn.expand("%:e")
+        local file = vim.fn.expand("%:p")
         local file_wo_ext = vim.fn.expand("%:p:r")
+        local file_dir = vim.fn.expand("%:p:h")
+        local cmd
         if ext == "py" then
-            keymap("n", "<Leader><F9>", cmd_prefix .. "python3 " .. file .. cmd_suffix, no_re_opts)
+            cmd = "python3 " .. file
         elseif ext == "cpp" then
-            keymap("n", "<Leader><F9>", cmd_prefix .. "g++ " .. file .. " -o " .. file_wo_ext .. ".o && " .. file_wo_ext .. ".o" .. cmd_suffix, no_re_opts)
+            cmd = "g++ " .. file .. " -o " .. file_wo_ext .. ".o && " .. file_wo_ext .. ".o"
         else
             keymap("n", "<Leader><F9>", "<NOP>", no_re_opts)
+            return
         end
+        RUN_FILE_TERM = require("toggleterm.terminal").Terminal:new {
+            cmd = cmd .. " ; exit",
+            dir = file_dir,
+            close_on_exit = false
+        }
+        keymap("n", "<Leader><F9>", ":w | lua RUN_FILE_TERM:toggle()<CR>", no_re_opts)
     end
 })
